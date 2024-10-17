381a08628c3fbea50e7ca798ae4d45da0d1c6244
KatieMari <katie.glover@student.norwichuni.ac.uk>
2024-10-17 04:00:43 -0700

    added play pause

--- Java/audio-player.js
+++ Java/audio-player.js
@@ -7,17 +7,30 @@ const playPauseButton = document.getElementById("play-button");
 //audioPlayer.src is the First song of the Audio Player by Default
 audioPlayer.src = "Assets/Songs/Song 1.mp3";
 
+
+let playing = false;
+
+
 /**
+ * if Audio Player is Playing -> Do Not Play Sound
  * if Audio Player is not Playing -> Play Sound
- * if Audio Player is Playing -. Do Not Play Sound
  */
 function onPlayPauseClick() {
-    audioPlayer.play();
+    if (playing) {
+        audioPlayer.pause();
+        playPauseButton.innerHTML = "Play";
+        playing = false;
+    } else {
+        audioPlayer.play();
+        playPauseButton.innerHTML = "Pause";
+        playing = true;
+    }
 }
 
-function onPlayPauseClick() {
-    audioPlayer.pause();
+function onLoadedMetadata(){
+    console.log(audioPlayer.duration);
 }
 
 // Link onclick Event to the onPlayPauseClick Button
 playPauseButton.onclick = onPlayPauseClick;
+audioPlayer.onloadedmetadata = onLoadedMetadata;
